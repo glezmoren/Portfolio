aaa2dfa616bc6b7e3d8b5bf4e969cb7c85d4978f
glezmoren <blancagonmor@gmail.com>
2022-05-31 11:43:12 +0200

    move responsive con cambios html y add href

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 
 
 
-// $(window).scroll(function(){
-//     let pixel    = $(window).scrollTop()
-//     let altoV    = $(window).height()
-//     let distCaja = $('.works__section').offset().top
+$(window).scroll(function(){
+    let pixel    = $(window).scrollTop()
+    let altoV    = $(window).height()
+    let distCaja = $('.works__section').offset().top
 
-//     if ( pixel >= distCaja - (altoV / 2) ){
+    if ( pixel >= distCaja - (altoV / 2) ){
 
-//         $('.works__section').addClass('activo')
-//     }
-// }
+        $('.works__section').addClass('activo')
+    }
+})
 
 
 $('.amarillo__dato.about').click(function(){
